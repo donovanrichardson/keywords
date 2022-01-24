9fb6fd9baa3c780b779ce075b2e2e5d13e3379f9
Donovan Richardson <donovanspqa@gmail.com>
2022-01-23 20:31:45 -0500

    text form works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TextFormComponent } from './text-form/text-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component:TextFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/text-form/text-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-form',
+  templateUrl: './text-form.component.html',
+  styleUrls: ['./text-form.component.css']
+})
+export class TextFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  model = ""
+
+}
